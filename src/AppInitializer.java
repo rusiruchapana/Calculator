625900c702d9e9d3db64688f9429a838e3a4e0db
rusiruchapana <rusiruchapana@gmail.com>
2023-11-22 23:44:59 +0530

    Designing part of the calculator.

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,10 +1,5 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -13,15 +8,7 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent parent = FXMLLoader.load(this.getClass().getResource("view/CalculateForm.fxml"));
-        Scene scene = new Scene(parent);
+    public void start(Stage primaryStage) {
 
-        scene.getStylesheets().add(getClass().getResource("styles/styles.css").toExternalForm());
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Calculator");
-        primaryStage.centerOnScreen();
-        primaryStage.show();
     }
 }
