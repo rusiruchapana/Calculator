bfe16da651c2bfe4be5f67d3ab00617ee0c5ee96
rusiruchapana <rusiruchapana@gmail.com>
2023-11-23 09:01:28 +0530

    Coding to the buttons.

--- src/controller/CalculatorFormController.java
+++ src/controller/CalculatorFormController.java
@@ -1,5 +1,125 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class CalculatorFormController {
 
+    public Button btnC;
+    public Button btnDel;
+    public Button btnModuler;
+    public Button btnDivide;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+    public Button btnMultiply;
+    public Button btn4;
+    public Button btn5;
+    public Button btn6;
+    public Button btnSubstract;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btnAddition;
+    public Button btnDecimal;
+    public Button btnEqual;
+    public Button btn0;
+    public TextField txtShow;
+
+
+
+    public void btnCOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+    
+
+    public void btnDeleteOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnModulesOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnDivideOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn7OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn8OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn9OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnMultiplyOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn4OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn5OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn6OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnSubstractOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn1OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn2OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btn3OnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnAdditionOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnDecimalOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnEqualOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void btnZeroOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
 }
